58b12d21eb72d61f11b957cf6148990132a8ce13
Jorge-Chamorro <jorgeorlandochamorro@gmail.com>
2023-01-20 14:38:38 -0300

    agregado del efefecto maquina de escribir en angular, usando la libreria t-writer

--- Pryecto-Portfolio/src/app/header/header.component.ts
+++ Pryecto-Portfolio/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+
+
   constructor() { }
 
   ngOnInit(): void {
+
+    const Typewriter = require('t-writer.js')
+
+    const target = document.querySelector('.tw');
+    
+    const writer = new Typewriter(target, {
+      loop: true, 
+      typeColor: 'white' 
+    })
+    
+    writer
+      .strings(
+        400,
+        "Soy Jorge Chamorro, un desarrollador web full stack",
+        "probando el t-write", 
+        "Using the 'strings' method"
+      )
+      .start()
+
+   
   }
 
 }
